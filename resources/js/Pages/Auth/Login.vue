a97d7da928dc62fad2d9fcbbaa02fc3238d275f4
Magicard <mice.graff@gmail.com>
2021-05-21 15:37:53 +0100

    First Comit

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,97 @@
+<template>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@900&display=swap" rel="stylesheet">
+    <jet-authentication-card>
+        <template #logo>
+            <inertia-link href="/" class="text-8xl title md:rounded-b-md mb-12 text-center mx-auto"
+                          style="font-family: 'Raleway', sans-serif !important;"> Well Of Screams
+            </inertia-link>
+        </template>
+
+        <jet-validation-errors class="mb-4" />
+
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+
+        <form @submit.prevent="submit">
+            <div>
+                <jet-label for="email" value="Email" />
+                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
+            </div>
+
+            <div class="mt-4">
+                <jet-label for="password" value="Password" />
+                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label class="flex items-center">
+                    <jet-checkbox name="remember" v-model:checked="form.remember" />
+                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <inertia-link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                    Forgot your password?
+                </inertia-link>
+
+                <button class="bg-blue-400 hover:bg-blue-300 text-white font-bold py-2 px-4 border-b-4
+                    border-blue-700 hover:border-blue-500 rounded-full text-center ml-auto" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Log in
+                </button>
+            </div>
+        </form>
+    </jet-authentication-card>
+</template>
+
+<script>
+    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard'
+    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo'
+    import JetButton from '@/Jetstream/Button'
+    import JetInput from '@/Jetstream/Input'
+    import JetCheckbox from '@/Jetstream/Checkbox'
+    import JetLabel from '@/Jetstream/Label'
+    import JetValidationErrors from '@/Jetstream/ValidationErrors'
+
+    export default {
+        components: {
+            JetAuthenticationCard,
+            JetAuthenticationCardLogo,
+            JetButton,
+            JetInput,
+            JetCheckbox,
+            JetLabel,
+            JetValidationErrors
+        },
+
+        props: {
+            canResetPassword: Boolean,
+            status: String
+        },
+
+        data() {
+            return {
+                form: this.$inertia.form({
+                    email: '',
+                    password: '',
+                    remember: false
+                })
+            }
+        },
+
+        methods: {
+            submit() {
+                this.form
+                    .transform(data => ({
+                        ... data,
+                        remember: this.form.remember ? 'on' : ''
+                    }))
+                    .post(this.route('login'), {
+                        onFinish: () => this.form.reset('password'),
+                    })
+            }
+        }
+    }
+</script>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,43 @@
+<template>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@900&display=swap" rel="stylesheet">
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="mx-auto w-8/12 font-black content-center">
+            <div class="flex flex-row flex-auto ">
+                <div class="text-8xl title md:rounded-b-md mb-12 text-center mx-auto"
+                     style="font-family: 'Raleway', sans-serif !important;"> Well Of Screams</div>
+            </div>
+            <div class="flex flex-row mb-5 w-1/4 mx-auto">
+                <inertia-link :href="route('login')"
+                    class="bg-blue-400 hover:bg-blue-300 text-white font-bold py-2 px-4 border-b-4
+                    border-blue-700 hover:border-blue-500 rounded-full flex-auto text-center">
+                    Log In
+                </inertia-link>
+            </div>
+            <div class="flex flex-row mb-5 w-1/4 mx-auto">
+                <inertia-link v-if="canRegister" :href="route('register')"
+                    class="bg-green-400 hover:bg-green-300 text-white font-bold py-2 px-4
+                    border-b-4 border-green-700 hover:border-green-500 rounded-full flex-auto text-center">
+                    Register
+                </inertia-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+html, body {
+
+}
+</style>
+
+<script>
+    export default {
+        props: {
+            canLogin: Boolean,
+            canRegister: Boolean,
+            laravelVersion: String,
+            phpVersion: String,
+        }
+    }
+</script>
